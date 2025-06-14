1d9ec3cb02554e52836822a0cff6945b9aefb6eb
Rico <rico2maldo@gmail.com>
2025-06-14 20:25:43 +0200

    test: deploy API v1.0.2 with simplified hook

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -1,4 +1,6 @@
 // src/server.js - version cohérente avec ton architecture
+// API Version 1.0.2 - Hook test deployment
+
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
