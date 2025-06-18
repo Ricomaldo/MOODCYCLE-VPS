5c20737d119e4ee04c272d3abfc4c2042ffdf0cd
Rico <rico2maldo@gmail.com>
2025-06-18 19:02:19 +0200

    test: admin hook

--- packages/admin/src/main.tsx
+++ packages/admin/src/main.tsx
@@ -8,3 +8,4 @@ console.log('Architecture VPS validée - Mon Jun 16 22:35:12 CEST 2025');
 // Admin test Wed Jun 18 18:41:18 CEST 2025
 // Admin test Wed Jun 18 18:42:48 CEST 2025
 // Admin test Wed Jun 18 18:53:09 CEST 2025
+// Admin test Wed Jun 18 19:02:19 CEST 2025
